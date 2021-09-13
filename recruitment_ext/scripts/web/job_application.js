6bbffe0604474bc4bf2584533b6e1aa2c2cedbac
av-dev2 <av-dev2@aakvatech.com>
2021-09-13 14:01:27 +0000

    feat: remove of aptitude test and put aptitude test template

--- recruitment_ext/scripts/web/job_application.js
+++ recruitment_ext/scripts/web/job_application.js
@@ -3,14 +3,14 @@ frappe.ready(() => {
 	const { job_title } = frappe.utils.get_query_params();
 	if (!job_title) return;
 
-	// If Job Opening has Aptitude Test, redirect to the respective Aptitude Test
+	// If Job Opening has Aptitude Test Template, redirect to the respective Aptitude Test Template
 	frappe.call({
-		method: 'recruitment_ext.www.aptitude_test.get_aptitude_test',
+		method: 'recruitment_ext.www.aptitude_test_template.get_aptitude_test_template',
 		args: { job_opening: job_title },
 		callback({ message }) {
 			if (!message) return;
 			frappe.web_form.handle_success = (doc) => {
-				window.location.assign(`/aptitude_test?job_applicant=${doc.name}&job_opening=${doc.job_title}`);
+				window.location.assign(`/aptitude_test_template?job_applicant=${doc.name}&job_opening=${doc.job_title}`);
 			};
 		}
 	});
