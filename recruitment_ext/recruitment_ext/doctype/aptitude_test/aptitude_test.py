0b3ce1f4d161c7ce517c7fb186101159b29472b1
av-dev2 <av-dev2@aakvatech.com>
2021-09-13 13:06:42 +0000

    feat: rename of aptitude test submission to aptitude test

--- recruitment_ext/recruitment_ext/doctype/aptitude_test/aptitude_test.py
+++ recruitment_ext/recruitment_ext/doctype/aptitude_test/aptitude_test.py
@@ -1,11 +1,35 @@
 # Copyright (c) 2021, Aakvatech Limited and contributors
 # For license information, please see license.txt
-import json
 
 import frappe
+from frappe.utils import flt
 from frappe.model.document import Document
 
-
 class AptitudeTest(Document):
-    def before_save(self):
-        self.total_points = sum(question.points for question in self.questions)
+	def validate(self):
+		self.calculate_result()
+		self.validate_total_earned_points()
+		self.set_total_earned_points_to_job_applicant()
+	
+	def on_update_after_submit(self):
+		self.validate_total_earned_points()
+	
+	def calculate_result(self):
+		self.total_earned_points = sum(answer.earned_points for answer in self.answers)
+		
+		if self.total_points:
+			self.result_percentage = flt(
+                self.total_earned_points / self.total_points * 100, 2
+            )
+	
+	def validate_total_earned_points(self):
+		if self.total_earned_points > self.total_points:
+			frappe.throw("Earned Points can not be greater than Total Points!")
+	
+	def set_total_earned_points_to_job_applicant(self):
+		frappe.db.set_value(
+            "Job Applicant", 
+            self.job_applicant,
+            'aptitude_test_score', 
+            self.total_earned_points
+        )
